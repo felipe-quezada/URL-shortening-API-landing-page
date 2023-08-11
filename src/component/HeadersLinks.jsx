1e15b3b465f1c4018077953c2b60611daa756729
Luis <fluis285@gmail.com>
2023-08-10 20:28:34 -0400

    commit 23-08-10

--- src/component/HeadersLinks.jsx
+++ src/component/HeadersLinks.jsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import '../styles/HeaderLinks.css';
 
 export const HeadersLinks = () => {
+	const [isClickedMenu, setIsClickedMenu] = useState(false);
+	const handleClickMenu = () => setIsClickedMenu(!isClickedMenu)
+
 	return (
 		<section className="headers-container global-container">
 			<img src="src\assets\logo.svg" alt="logo" />
@@ -16,15 +20,23 @@ export const HeadersLinks = () => {
 				</section>
 			</nav>
 			<nav className="headers-container__menu-responsive">
-				<ul className="headers-container__left">
+				<div
+					className={`headers-container__arrow-button ${
+						isClickedMenu ? 'clicked' : 'unclicked'
+					}`}
+					onClick={handleClickMenu}
+				>
+					<strong> {'<'} </strong>
+				</div>
+				<ul className={`headers-container__menu-content ${isClickedMenu ? 'visible' :'hidden'}`}>
+					<a href="#">Login</a>
+					<a href="#">Resources</a>
 					<a href="#">Features</a>
 					<a href="#">Pricing</a>
-					<a href="#">Resources</a>
+					<a>Sign up</a>
 				</ul>
-				<section className="headers-container__right">
-					<a>Login</a>
-					<button className="headers-conainer__button">Sign Up</button>
-				</section>
+				<div className={`headers-container__bar-menu ${isClickedMenu ? 'visible' :'hidden'}`}></div>
+				<section className="headers-container__right"></section>
 			</nav>
 		</section>
 	);
