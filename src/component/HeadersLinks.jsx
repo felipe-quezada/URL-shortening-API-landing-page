2b014747b6db60985804ab01b3c8bcd788b84e61
Luis <fluis285@gmail.com>
2023-08-14 18:22:40 -0400

    commit 23-08-14 header

--- src/component/HeadersLinks.jsx
+++ src/component/HeadersLinks.jsx
@@ -1,9 +1,27 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import '../styles/HeaderLinks.css';
 
 export const HeadersLinks = () => {
-	const [isClickedMenu, setIsClickedMenu] = useState(false);
-	const handleClickMenu = () => setIsClickedMenu(!isClickedMenu)
+	const menuState = {
+		isClicked: false,
+		menuClass: 'hidden',
+		burger: 'unclicked',
+	};
+	const reducer = (menu, action) => {
+		if (action.type == 'clicked') {
+			return {
+				isClicked: !menu.isClicked,
+				menuClass: menu.isClicked ? 'hidden' : 'visible',
+				burger: menu.isClicked ? 'unclicked' : 'clicked',
+			};
+		}
+		throw Error('Unknown action');
+	};
+	const [menu, setMenu] = useReducer(reducer, menuState);
+
+	const handleClick = () => {
+		setMenu({ type: 'clicked' });
+	};
 
 	return (
 		<section className="headers-container global-container">
@@ -18,6 +36,21 @@ export const HeadersLinks = () => {
 					<a>Login</a>
 					<button className="headers-conainer__button">Sign Up</button>
 				</section>
+			</nav>
+			<nav className="headers-container__menu-responsive">
+				<div onClick={handleClick} className={`burger-menu ${menu.burger}`}>
+					<div className="burger-bar"></div>
+					<div className="burger-bar"></div>
+					<div className="burger-bar"></div>
+				</div>
+				<nav className={`nav-menu ${menu.menuClass}`}>
+					<a href="#">login</a>
+					<a href="#">Pricing</a>
+					<a href="#">Features</a>
+					<a href="#">Resources</a>
+					<a href="#">Sign in</a>
+					<a>{String(menu.isClicked)}</a>
+				</nav>
 			</nav>
 		</section>
 	);
