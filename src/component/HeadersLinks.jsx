ef4d6e0d6c29da29e56c55f92e356716be45d7ad
Luis <fluis285@gmail.com>
2023-08-17 13:39:02 -0400

    commit header fix 23-08-17

--- src/component/HeadersLinks.jsx
+++ src/component/HeadersLinks.jsx
@@ -1,21 +1,22 @@
 import { useReducer } from 'react';
 import '../styles/HeaderLinks.css';
 
+const reducer = (menu, action) => {
+	if (action.type == 'clicked') {
+		return {
+			isClicked: !menu.isClicked,
+			menuClass: menu.isClicked ? 'hidden' : 'visible',
+			burger: menu.isClicked ? 'unclicked' : 'clicked',
+		};
+	}
+	throw Error('Unknown action');
+};
+
 export const HeadersLinks = () => {
 	const menuState = {
 		isClicked: false,
 		menuClass: 'hidden',
 		burger: 'unclicked',
-	};
-	const reducer = (menu, action) => {
-		if (action.type == 'clicked') {
-			return {
-				isClicked: !menu.isClicked,
-				menuClass: menu.isClicked ? 'hidden' : 'visible',
-				burger: menu.isClicked ? 'unclicked' : 'clicked',
-			};
-		}
-		throw Error('Unknown action');
 	};
 	const [menu, setMenu] = useReducer(reducer, menuState);
 
@@ -24,34 +25,39 @@ export const HeadersLinks = () => {
 	};
 
 	return (
-		<section className="headers-container global-container">
-			<img src="src\assets\logo.svg" alt="logo" />
-			<nav className="headers-container__menu global-container">
-				<ul className="headers-container__left">
-					<a href="#">Features</a>
-					<a href="#">Pricing</a>
-					<a href="#">Resources</a>
-				</ul>
-				<section className="headers-container__right">
-					<a>Login</a>
-					<button className="headers-conainer__button">Sign Up</button>
-				</section>
-			</nav>
-			<nav className="headers-container__menu-responsive">
-				<div onClick={handleClick} className={`burger-menu ${menu.burger}`}>
-					<div className="burger-bar"></div>
-					<div className="burger-bar"></div>
-					<div className="burger-bar"></div>
-				</div>
-				<nav className={`nav-menu ${menu.menuClass}`}>
-					<a href="#">login</a>
-					<a href="#">Pricing</a>
-					<a href="#">Features</a>
-					<a href="#">Resources</a>
-					<a href="#">Sign in</a>
-					<a>{String(menu.isClicked)}</a>
+		<>
+			<section className="headers-container global-container">
+				<img src="src\assets\logo.svg" alt="logo" />
+				<nav className="headers-container__menu global-container">
+					<ul className="headers-container__left">
+						<a href="#">Features</a>
+						<a href="#">Pricing</a>
+						<a href="#">Resources</a>
+					</ul>
+					<section className="headers-container__right">
+						<a>Login</a>
+						<button className="headers-conainer__button">Sign Up</button>
+					</section>
 				</nav>
-			</nav>
-		</section>
+
+				<nav className="headers-container__menu-responsive">
+					<div onClick={handleClick} className={`burger-menu ${menu.burger}`}>
+						<div className="burger-bar"></div>
+						<div className="burger-bar"></div>
+						<div className="burger-bar"></div>
+					</div>
+				</nav>
+			</section>
+			<section className={`nav global-container ${menu.menuClass}`}>
+				<nav className={'nav-menu'}>
+					<a href="#">Features</a>
+					<a href="#">Pricing</a>
+					<a href="#">Resources</a>
+					<hr />
+					<a href="#">Login</a>
+					<button>Sign Up</button>
+				</nav>
+			</section>
+		</>
 	);
 };
